d267487637501e6334138015fe3ca7a16e6e3479
afrizal-10 <adijuliyantoafrizal10@gmail.com>
2025-07-27 18:37:18 +0700

    fix bug deploy

--- server.js
+++ server.js
@@ -18,7 +18,4 @@ app.use(express.json());
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app; // ekspor app saja, tanpa listen
